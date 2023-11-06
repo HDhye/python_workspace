e3e5d3bf48e64cd0d6672a4604780d5b3251dc7c
Dh H <hdhye89@gmail.com>
2023-11-06 17:16:40 +0900

    동적 뷰 라우팅 로직

--- DJANGO_LECTURE/my_site2/first_app/urls.py
+++ DJANGO_LECTURE/my_site2/first_app/urls.py
@@ -5,5 +5,12 @@ from . import views # 동일한 폴더 views 연결
 urlpatterns = [
     # /first_app/ 비워두면 프로젝트 레벨에서 정의된 모든것에 추가됨 
     # path('', views.simple_view)
-    path('simple_view', views.simple_view),
+    # path('simple_view', views.simple_view),
+    # path('sports/', views.sports_view), #http://127.0.0.1:8000/first_app/sports/
+    # path('finance/', views.finance_view) #http://127.0.0.1:8000/first_app/finance/
+    
+    # 동적 url 라우팅 설정 
+    path('<str:topic>/', views.news_view) # string 타입 설정, 비워두면 모든타입 
+    , 
+    path('<int:num1>/<int:num2>', views.add_view)
 ]
--- DJANGO_LECTURE/my_site2/first_app/views.py
+++ DJANGO_LECTURE/my_site2/first_app/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
 # Create your views here.
 
 
-def simple_view(request):
-    return HttpResponse("simple view page")
+# def simple_view(request):
+#     return HttpResponse("simple view page")
 
+# 동적 뷰 url 설정 
 
-def home_view(request):
-    return HttpResponse("HOME VIEW~")
+articles = { 
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'
+}
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    # domain.com/first_app/num1/num2 --> num1+num2 
+    addrst = num1 + num2 
+    result = f"{num1} + {num2} = {addrst}"
+    return HttpResponse(str(result)) 
