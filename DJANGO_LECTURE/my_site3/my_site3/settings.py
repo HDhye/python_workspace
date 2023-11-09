5ad141e4eef0f9a73cf0da69ee493ed959ea611d
Dh H <hdhye89@gmail.com>
2023-11-09 16:13:00 +0900

    static 파일 연결

--- DJANGO_LECTURE/my_site3/my_site3/settings.py
+++ DJANGO_LECTURE/my_site3/my_site3/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-v)5b_vns@o(7%wq79lf@w@jhp!aqpd#-gr+f3$%flp&vpsxejc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -114,10 +116,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) 정적파일 등록 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+# my_app/static/my_app/image1.png
+STATIC_URL = "static/" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
