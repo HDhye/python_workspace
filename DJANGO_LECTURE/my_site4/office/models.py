483a4e0d2dbcbe846f7eff8e719c9ea1afb1c13f
Dh H <hdhye89@gmail.com>
2023-11-17 13:50:34 +0900

    db 조회 및 str 오버라이드

--- DJANGO_LECTURE/my_site4/office/models.py
+++ DJANGO_LECTURE/my_site4/office/models.py
@@ -6,3 +6,8 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField() # 유효성 검사 설정 
+    
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name} is {self.age} years old."
+    
+    
