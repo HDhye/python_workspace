e8299b71ddf6296695ae7821958e0da19836fb77
Dh H <hdhye89@gmail.com>
2023-12-04 15:37:55 +0900

    admin model 연결

--- DJANGO_LECTURE/my_car_site/cars/admin.py
+++ DJANGO_LECTURE/my_car_site/cars/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from cars.models import Car  # app - model 임포트 
 
-# Register your models here.
+# admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):  # model Name + Admin 
+    # 필드 순서 변경 
+    fields  = ['year', 'brand']
+    
+    # 필드셋 - 세그먼트화 
+    # fieldsets = [
+    #     ('TIME INFORMATION', {'fields' : ['year']}),
+    #     ('CAR INFORMATION', {'fields': ['brand']}) 
+    # ]
+admin.site.register(Car, CarAdmin)
