2678b1c76e06bf53665ae0fd936db4092fa0ad70
Dh H <hdhye89@gmail.com>
2023-12-14 12:55:39 +0900

    CBV 모델폼 삭제 기능

--- DJANGO_LECTURE/school/classroom/views.py
+++ DJANGO_LECTURE/school/classroom/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 
 # 폼뷰를 일반뷰(generic view)로 임포트 
 from django.views.generic import (TemplateView, FormView, 
-CreateView, ListView, DetailView, UpdateView)
+CreateView, ListView, DetailView, UpdateView, DeleteView)
 
 # 폼을 뷰에 임포트 
 from classroom.forms import ContactForm
@@ -42,16 +42,22 @@ class ContractFromView(FormView):
 
 # Create View 
 class TeacherCreateView(CreateView):
+    # model_form.html 연결 
+    
     model = Teacher # 사용 모델  
     
     # 모델뷰에서는 submit 버튼을 누르면 save() 동작을 한다.
     fields = "__all__"
+    
+    # 성공시 반환 주소 
     success_url = reverse_lazy("classroom:thank_you")
     
 # List View 
 class TeacherListView(ListView):
-    # model_list.html
-    model = Teacher # 모델 인스턴스 연결 
+    # model_list.html 연결 
+    
+    model = Teacher # 모델 인스턴스  
+    
     # 쿼리속성 재정의(오버라이드)
     queryset = Teacher.objects.order_by('first_name') # 정렬 
     
@@ -60,21 +66,28 @@ class TeacherListView(ListView):
 
 # Detail View 
 class TeacherDetailView(DetailView):
-    # 하나의 모델 아이템만 반환
-    
     # model_detail.html
+    
     model = Teacher 
     
+    # 하나의 모델 아이템만 반환
     # pk --> {{ teacher }}
      
 # Update View 
 class TeacherUpdateView(UpdateView):
-    model = Teacher 
+    # form -> 디테일뷰 기반 템플릿 자동 연결함 
     
+    model = Teacher 
     # fields = "__all__" # 전체 가져오기 
-    fields = ['last_name', 'subject'] # 이름과 과목만 수정 가능 
+    fields = ['last_name', 'subject'] # 이름과 과목만 수정 가능하게 폼 설정  
     
     success_url = reverse_lazy("classroom:list_teacher")
     
     
+# Delete View
+class TeacherDeleteView(DeleteView):
+    # form --> 삭제확인 버튼 폼 = model_confirm_delete.html 자동연결
+    model = Teacher 
+    
+    success_url = reverse_lazy("classroom:list_teacher")
     
